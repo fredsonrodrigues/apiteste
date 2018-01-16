9c8181cb24d88b16de1270e2f5680fcc8d7b5ede
Fredson Rodrigues <fredson.jorge@ceuma.br>
2018-01-16 17:59:10 -0300

    Mudando para a porta 80

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.JS!');
-}).listen(8081);
+}).listen(80);
 
-console.log('Server running at http://localhost:8080/');
+console.log('Server running');
